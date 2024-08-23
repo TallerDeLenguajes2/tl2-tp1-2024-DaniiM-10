38ca21385b1dabf8c6b0c2841cf108ddccaa9cff
DaniiM-10 <daniimonteros1@gmail.com>
2024-08-23 19:43:02 -0300

    Creo el archivo con la clase Cliente

---
+++ Cliente.cs
@@ -0,0 +1,13 @@
+namespace tp1;
+public class Cliente
+{
+    private string nombre;
+    private string direccion;
+    private string telefono;
+    private string datosReferenciaDireccion;
+
+    public string Nombre { get => nombre; set => nombre = value; }
+    public string Direccion { get => direccion; set => direccion = value; }
+    public string Telefono { get => telefono; set => telefono = value; }
+    public string DatosReferenciaDireccion { get => datosReferenciaDireccion; set => datosReferenciaDireccion = value; }
+}
